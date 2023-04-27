f90fb0c7bdeddad24d94e6a1bf810f12919c54bf
Dostonbek <dostonbek@gmail.com>
2023-04-27 11:49:37 +0500

    added only g

--- app/src/main/java/com/developer/hamkorauth/fragment/BlankFragment2.kt
+++ app/src/main/java/com/developer/hamkorauth/fragment/BlankFragment2.kt
@@ -33,7 +33,7 @@ class BlankFragment2 : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View? {g
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_blank2, container, false)
     }
